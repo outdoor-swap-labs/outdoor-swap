b25cc64b3efd4ff6fe721c0924a83786423389a7
Giovana De La Cruz <giovanacdlc@hotmail.com>
2023-05-22 16:39:10 -0700

    started database

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,14 @@
 const userController = {};
 
+//post request to create a user
+userController.signUp = (req, res, next) => {
+  const { username, password } = req.body;
+  const createUserQuery = 
+
+}
+
+
+// login middleware
 userController.login = (req, res, next) => {
     const { username, password } = req.body;  
     if (username === '' && password === '') {
