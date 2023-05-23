f83d6c343c4feab8b1500e8c2b1f6950213f880c
Alejandro Florez <121457814+AlejandroFlorez@users.noreply.github.com>
2023-05-23 13:24:44 -0400

    Set webpack for proxy and create navbar components
    Co-authored-by: Christina Walton <CElizOwens@users.noreply.github.com>

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import MainNavBar from './components/MainNavBar';
 import './assets/styles.scss';
 
+const id = 1;
+
 function App(props) {
+  const [userName, setUserName] = useState('');
+
+  useEffect(async () => {
+    try {
+      const response = await fetch(`/user/${id}`);
+      const data = await response.json();
+      setUserName(data.userName);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
-    <div>
-      <h1>Welcome to the App component!</h1>
-    </div>
+    <>
+      <MainNavBar userName={userName} />
+      <div>
+        <h1>Welcome to the App component!</h1>
+      </div>
+    </>
   );
 }
 
