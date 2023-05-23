0da5498d5aa2a892f5508ceb60b33e049bcf47f9
Sophia Bui <sophiaxbui@gmail.com>
2023-05-23 14:35:53 -0700

    testing available sql column, success
    Co-authored-by: Giovana De La Cruz <giovanacdlc@users.noreply.github.com>
    Co-authored-by: Matteo <diterlizzi.matteo@gmail.com>

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -4,13 +4,13 @@ const itemController = {};
 // filter by category
 itemController.getByCategory = (req, res, next) => {
     const category= req.params.category;
-    const categoryQuery = 'SELECT * FROM "public"."item" WHERE category = $1';
+    const categoryQuery = 'SELECT * FROM "public"."item" WHERE category = $1 AND available = true';
     const categoryItemsVar = [category]
     db.query(categoryQuery, categoryItemsVar)
       .then(data => {
         console.log(data)
         if (data.rows.length === 0) {
-          throw new Error('No item found in that category');
+          throw new Error('No available item found in that category');
         }
         res.locals.items = data.rows;
         return next();
@@ -24,13 +24,13 @@ itemController.getByCategory = (req, res, next) => {
 // filter by zipcode
 itemController.getByLocation = (req, res, next) => {
     const location= req.params.location;
-    const locationQuery = 'SELECT * FROM "public"."item" WHERE location = $1';
+    const locationQuery = 'SELECT * FROM "public"."item" WHERE location = $1 AND available = true';
     const locationItemsVar = [location]
     db.query(locationQuery, locationItemsVar)
       .then(data => {
         console.log(data)
         if (data.rows.length === 0) {
-          throw new Error('No item found in that location');
+          throw new Error('No available item found in that location');
         }
         res.locals.items = data.rows;
         return next();
