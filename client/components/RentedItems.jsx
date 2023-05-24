947ea09a9925ec6d5b9c30f492fb88d1aa70e967
Sophia Bui <sophiaxbui@gmail.com>
2023-05-24 12:06:57 -0700

    rentedItems sass file created effects and cards
    Co-authored-by: Giovana De La Cruz <giovanacdlc@users.noreply.github.com>
    Co-authored-by: Matteo <diterlizzi.matteo@gmail.com>

--- client/components/RentedItems.jsx
+++ client/components/RentedItems.jsx
@@ -1,14 +1,6 @@
-// import React from 'react';
-
-// function RentedItems() {
-//   return <div>These are the RentedItems</div>;
-// }
-
-// export default RentedItems;
-
 // function that fetches the item id of that user reservation
-
 import React, { useState, useEffect } from 'react';
+import '../assets/scss/RentedItems.scss';
 
 function RentedItems() {
   const [items, setItems] = useState([]);
@@ -29,21 +21,43 @@ function RentedItems() {
 
   return (
     <div>
+      <h1>These are your Rented Items!</h1>
+      <div className="space">
+      </div>
       {items.map((item, index) => (
-        <div key={index}>
-          <img src={item.photo} alt={item.description} />
-          <p>Category: {item.category}</p>
-          <p>Size: {item.size}</p>
+        <ul>
+          <li>
+        <div key={index} className="details">
+          <h2>{item.description}</h2>
           <p>Price: {item.price}</p>
+          {/* <p>Category: {item.category}</p>
+          <p>Size: {item.size}</p>
           <p>Location: {item.location}</p>
           <p>Reserved Date: {item.date_reserved}</p>
-          <p>Return Date: {item.date_returned}</p>
+          <p>Return Date: {item.date_returned}</p> */}
+          <div class="product">
+          <img src={item.photo} alt={item.description} />
+          </div>
           {/* <p>Available: {item.available ? 'Yes' : 'No'}</p> */}
-          <p>Description: {item.description}</p>
         </div>
+        </li>
+        </ul>
       ))}
     </div>
-  );
+);
 }
+
+{/* <ul>
+  <li>
+    <div class="details">
+      <h2>Team Rainbow Unicorn</h2>
+      <p>$25</p>
+      <div class="product">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/881020/threadless01.jpg">
+      </div>
+    </div>
+  </li>
+  <li>
+  <ul> */}
 
 export default RentedItems;
